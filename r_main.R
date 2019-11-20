1245a840157db1b34fd04ece83343404cec97e83
RobinKohrs <robin.kohrs@gmx.de>
2019-11-20 17:56:50 +0100

    clip hamburg

--- r_main.R
+++ r_main.R
@@ -22,9 +22,14 @@ if (!file.exists(destfile)) {
   }
 
 # Extract Zip-File -------------------------------------------------------------
-unzip(
-  zipfile = destfile,
-  exdir = "data/germany")
+if (!file.exists("data/germany")){
+  unzip(
+    zipfile = destfile,
+    exdir = "data/germany")
+} else {
+  print("also already zipped")
+}
+
 
 # Read Shapefile
 germany = st_read("data/germany/VG250_Bundeslaender.shp")
@@ -34,7 +39,9 @@ str(germany)
 head(germany$GEN)
 
 # PLot the shapefile
-plot(st_geometry(germany))
+ggplot(germany) +
+  geom_sf(data = germany, col = "black", fill = "lightblue") +
+  ggtitle("Deutschland in WGS84")
 
 # Plot Hamburg
 hamburg = germany %>%
@@ -50,7 +57,11 @@ ggplot(hamburg) +
   coord_sf(crs = st_crs(hamburg)) +
   ggtitle("Hamburg", subtitle = "und seine Inseln")
 
-# draw a polygon in the southeastern corner of the meuse
+ggplot(data = hamburg) +
+  geom_sf( color = "black", fill = "blue") +
+  coord_sf(xlim = c(9.5, 10.5), ylim = c(53.3, 53.8), expand = TRUE)
+
+# draw a polygon in the southeastern corner of the map
 poly = data.frame(x = c(9.5, 10.5, 10.5, 9.5),
                   y = c(53.3, 53.3, 53.8, 53.8))
 
@@ -61,6 +72,13 @@ poly = st_polygon(list(poly)) %>%
 # convert simple feature into a sfc
   st_sfc(crs = 4326)
 
-class(poly)
+st_crs(poly)
+
+plot(hamburg$geometry)
+
+# add polygon
+plot(poly, add = TRUE,
+     border = "red",
+     lwd = 2)
 
 
